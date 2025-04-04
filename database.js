7bb26c8b641ca054cf5cee0ced415fc412dd2341
Bharat Shah <shahbharat2609@gmail.com>
2025-04-05 00:20:14 +0530

    performed some methods on the collection for practice using mongodb

--- database.js
+++ database.js
@@ -1,3 +1,47 @@
-// import mongodb from "mongodb";
+import { MongoClient } from "mongodb";
 
-const URI = "mongodb+srv://namaste-node:Shahbharat@7@namaste-node.dr0rdup.mongodb.net/"
+const url = "mongodb+srv://namaste-node:Shahbharat%407@namaste-node.dr0rdup.mongodb.net/?retryWrites=true&w=majority&appName=namaste-node";
+const dbName = "HelloWorld";
+
+const client = new MongoClient(url);
+
+const connectDB = async () => {
+  try {
+    await client.connect();
+    console.log("✅ Connected successfully to MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection("User");
+
+    const data = {
+      firstName: "John",
+      lastName: "Doe",
+      city: "GTA Vice City",
+      phoneNumber: "9876543210",
+    }
+    // const insertDocument = await collection.insertOne(data);
+    // console.log("Inserted document =>: ", insertDocument);
+
+    const findResult = await collection.find({}).toArray();
+    console.log("Found documents =>: ", findResult);
+
+    const countResult = await collection.countDocuments();
+    console.log('Documents count =>: ', countResult);
+
+
+  //find all documents with the firstname - John
+    const findNameResult = await collection.find({ firstName: "John" }).toArray();
+    console.log("findNameResult =>: ", findNameResult)
+
+
+
+
+    return collection;
+  } catch (error) {
+    console.error("❌ Connection failed:", error);
+  } finally {
+    await client.close();
+  }
+};
+
+connectDB();
